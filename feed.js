4c76cd12edb765b06c7fad2ecce14c601ccb1d49
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2015-02-14 20:13:50 +0000

    fix feed generator

--- feed.js
+++ feed.js
@@ -4,39 +4,51 @@ module.exports = function() {
     , RSS = require("rss")
     , moment = require("moment")
     , fs = require("fs")
-    , root_url = harp.root_url[process.env.NODE_ENV]
-    , image_root_url = root_url + "assets/images/"
-    , output_feed = "public/rss.xml"
+    , rootUrl = harp.root_url[process.env.NODE_ENV]
+    , imageRootUrl = rootUrl + "assets/images/"
+    , outputFeed = "public/rss.xml"
+    , books = []
   ;
 
   var feed = new RSS({
     title: harp.title,
     description: harp.description,
-    feed_url: root_url + "rss.xml",
-    site_url: root_url,
-    image_url: image_root_url + "devfreebooks-128.png",
+    feed_url: rootUrl + "rss.xml",
+    site_url: rootUrl,
+    image_url: imageRootUrl + "devfreebooks-128.png",
     managingEditor: harp.author,
     pubDate: moment().format("LLLL"),
     ttl: 60
   });
   
-  harp.platforms.forEach(function(platform_name) {
-    var platform = require("./public/"+ platform_name +"/_data.json")["index"];
+  harp.platforms.forEach(function(platformName) {
+    var platform = require("./public/"+ platformName +"/_data.json")["index"];
 
     platform.books.forEach(function(book) {
-      var book_id = book.title.replace(/[^\w\s]/g, "").replace(/\s/g, "-").toLowerCase();
-      var book_link = root_url + platform_name + "/#" + book_id;
-      var book_published_at = moment(book.published_at || moment().format("YYYYMMDD"), "YYYYMMDD");
+      var bookId = book.title.replace(/[^\w\s]/g, "").replace(/\s/g, "-").toLowerCase();
+      var bookLink = rootUrl + platformName + "/#" + bookId;
+      var bookPublishedAt = moment(book.published_at || moment().format("YYYYMMDD"), "YYYYMMDD");
 
-      feed.item({
+      books.push({
         title: "Book: " + book.title,
         description: book.description,
-        url: book_link,
+        url: bookLink,
         author: harp.author,
-        date: book_published_at.format("ll")
+        date: bookPublishedAt.format("ll")
       });
     });
   });
-  fs.writeFileSync(output_feed, feed.xml());
-  console.log("Generated RSS:", output_feed);
+
+  var sortedBooks = books.sort(function(a, b) {
+    var a = moment(a.date, "ll").toDate().getTime();
+    var b = moment(b.date, "ll").toDate().getTime();
+    if (a < b) return 1;
+    if (a > b) return -1;
+    return 0;
+  });
+
+  sortedBooks.forEach(function(book) { feed.item(book); });
+
+  fs.writeFileSync(outputFeed, feed.xml());
+  console.log("Generated RSS:", outputFeed);
 };
